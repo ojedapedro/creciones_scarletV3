a350ddcd56e6c9328d71a68ce0017fe6546cb81b
ANALISTA DE DATOS <ojedapedro2302741@gmail.com>
2025-12-03 18:37:59 -0400

    modificado1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
